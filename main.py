5a4e7eb44d2b83fac40a75feeebf2ad7704286ea
srinivasjs272 <srinivasarao.j@winwire.com>
2023-06-27 12:11:39 +0530

    New files

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ aws_access_key_id=aws_access_key_id,
 aws_secret_access_key=aws_secret_access_key,
 region_name=aws_region # Replace with your desired AWS region
 )
-
+#adding this for testing purpose
 
 
 def list_s3_buckets():
